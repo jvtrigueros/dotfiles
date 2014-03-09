7e1d664ff72fb7a19a99d8f4dc65d48d26f41686
Jose V. Trigueros <j.v.trigueros@gmail.com>
2014-03-09 15:32:10 -0600

    Changing the order in with the Bundle command is run.

--- setup-vim.sh
+++ setup-vim.sh
@@ -2,6 +2,5 @@
 sudo apt-get install vim
 
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-vim +BundleInstall +qall
-
 ln -s $PWD/vimrc.symlink $HOME/.vimrc
+vim +BundleInstall +qall
