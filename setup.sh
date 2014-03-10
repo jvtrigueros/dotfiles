65171fa766ad6846445c5635c83e8576e18b4a6f
Jose V. Trigueros <j.v.trigueros@gmail.com>
2014-03-10 01:29:47 -0600

    Just adding some general setup.

---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git clone https://github.com/chriskempson/base16-gnome-terminal.git $HOME/.base16
+chmod +x $HOME/.base16/base16-solarized.dark.sh
+$HOME/.base16/base16-solarized.dark.sh
