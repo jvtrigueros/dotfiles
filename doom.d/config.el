7819c86b7c7edf9701952b7c8185f7c594bca9db
Jose V. Trigueros <j.v.trigueros@gmail.com>
2021-06-10 12:39:09 -0700

    Add emacs configuration

---
+++ doom.d/config.el
@@ -0,0 +1,120 @@
+;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Jose V. Trigueros"
+      user-mail-address "")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
+;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+(setq doom-font (font-spec :family "Fantasque Sans Mono" :size 14 :weight 'normal)
+      doom-variable-pitch-font (font-spec :family "Fantasque Sans Mono" :size 14))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-nord-light)
+;; (setq doom-theme 'doom-dracula)
+;; (setq doom-theme 'doom-one-light)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+(add-load-path! "./elisp")
+
+(use-package! super-save
+  :config
+  (super-save-mode +1))
+
+(use-package! beacon
+  :after doom-themes
+  :config
+  (beacon-mode +1)
+  (setq beacon-color (let ((bg (face-attribute 'highlight :background nil t)))
+                       (if (eq bg 'unspecified)
+                           (face-attribute 'highlight :foreground nil t)
+                         bg))
+        beacon-blink-when-buffer-changes t
+        beacon-blink-when-point-moves-vertically 10))
+
+(use-package! engine-mode
+  :config
+  (engine-mode t)
+  (map! :leader
+        (:prefix-map ("s" . "search")
+         :desc "engine-mode" "e" #'engine-mode-prefixed-map))
+  (load "engines"))
+
+(after! magit
+  (setq auth-sources '("~/.authinfo")))
+
+;; Change default avy-keys to be DVORAK friendly
+(setq avy-keys '(?a ?o ?e ?u ?i ?d ?h ?t ?n))
+
+;; Change Default Formatter for Clojure
+(setq-hook! 'clojure-mode-hook +format-with-lsp nil)
+(set-formatter! 'cljstyle "cljstyle pipe" :modes '(clojure-mode))
+
+(after! clojure-mode
+  (setq! clojure-align-separator 'entire))
+
+;; Add push git options
+(map! :leader
+      (:prefix-map ("g" . "git")
+       (:when (featurep! :tools magit)
+        :desc "Push" "p" #'magit-push)))
+
+(after! git-rebase-mode
+  (map!
+   :map git-rebase-mode-map
+   "S-SPC" #'git-rebase-show-or-scroll-up))
+
+;; Still looking for a way to make this work, commenting in the meantime
+;; (after! lispyville-mode
+;;   (lispyville-mode-line-string))
+
+;; macOS
+(when IS-MAC
+  (setq! mac-right-option-modifier 'meta)
+
+  ;; Configure the location of Omnisharp for lsp server
+  (setq! omnisharp-server-executable-path "/usr/local/bin/omnisharp"))
+
+;; Load keys and certificates from auth-sources
+(setq network-stream-use-client-certificates t)
+
