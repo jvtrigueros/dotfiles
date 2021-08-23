2f7b13bb8058261ae7fb1b40c7c168f11f1fe8bf
Jose V. Trigueros <j.v.trigueros@gmail.com>
2021-08-23 10:33:15 -0700

    Disable lispy and enable parinfer-rust

--- doom.d/init.el
+++ doom.d/init.el
@@ -52,18 +52,18 @@
        ;;zen                  ; distraction-free coding or writing
 
        :editor
-       (evil +everywhere)     ; come to the dark side, we have cookies
-       file-templates         ; auto-snippets for empty files
-       fold                   ; (nigh) universal code folding
-       format                 ; automated prettiness
-       ;;god                  ; run Emacs commands without modifier keys
-       lispy                  ; vim for lisp, for people who don't like vim
-       multiple-cursors       ; editing in many places at once
-       ;;objed                ; text object editing for the innocent
-       ;; (parinfer +rust)    ; turn lisp into python, sort of
-       ;;rotate-text          ; cycle region at point between text candidates
-       snippets               ; my elves. They type so I don't have to
-       ;;word-wrap            ; soft wrapping with language-aware indent
+       (evil +everywhere)  ; come to the dark side, we have cookies
+       file-templates      ; auto-snippets for empty files
+       fold                ; (nigh) universal code folding
+       format              ; automated prettiness
+       ;;god               ; run Emacs commands without modifier keys
+       ;; lispy            ; vim for lisp, for people who don't like vim
+       multiple-cursors    ; editing in many places at once
+       ;;objed             ; text object editing for the innocent
+       (parinfer +rust)    ; turn lisp into python, sort of
+       ;;rotate-text       ; cycle region at point between text candidates
+       snippets            ; my elves. They type so I don't have to
+       ;;word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
        (dired +icons +ranger) ; making dired pretty [functional]
