c144c03e2d919826facde93845c4cb8142265174
Jose V. Trigueros <j.v.trigueros@gmail.com>
2021-08-23 10:36:58 -0700

    Disable scala

--- doom.d/init.el
+++ doom.d/init.el
@@ -140,27 +140,27 @@
        ;;latex             ; writing papers in Emacs has never been so fun
        ;;lean
        ;;factor
-       ;;ledger               ; an accounting system in Emacs
-       ;;lua                  ; one-based indices? one-based indices
-       (markdown +grip)       ; writing docs for people to ignore
-       ;;nim                  ; python + lisp at the speed of c
-       ;;nix                  ; I hereby declare "nix geht mehr!"
-       ;;ocaml                ; an objective camel
-       org                    ; organize your plain life in plain text
-       ;;php                  ; perl's insecure younger brother
-       ;;plantuml             ; diagrams for confusing people more
-       ;;purescript           ; javascript, but functional
-       ;;(python +lsp +pyright +pyenv)            ; beautiful is better than ugly
-       ;;qt                   ; the 'cutest' gui framework ever
-       ;;racket               ; a DSL for DSLs
-       ;;raku                 ; the artist formerly known as perl6
-       rest                             ; Emacs as a REST client
-       ;;rst                  ; ReST in peace
-       ;;(ruby +rails)        ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       ;;rust                 ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
-       ;;scala                ; java, but good
-       ;;scheme               ; a fully conniving family of lisps
-       sh                     ; she sells {ba,z,fi}sh shells on the C xor
+       ;;ledger            ; an accounting system in Emacs
+       ;;lua               ; one-based indices? one-based indices
+       markdown            ; writing docs for people to ignore
+       ;;nim               ; python + lisp at the speed of c
+       ;;nix               ; I hereby declare "nix geht mehr!"
+       ;;ocaml             ; an objective camel
+       org                 ; organize your plain life in plain text
+       ;;php               ; perl's insecure younger brother
+       ;;plantuml          ; diagrams for confusing people more
+       ;;purescript        ; javascript, but functional
+       ;; (python +lsp +pyright +pyenv)            ; beautiful is better than ugly
+       ;;qt                ; the 'cutest' gui framework ever
+       ;;racket            ; a DSL for DSLs
+       ;;raku              ; the artist formerly known as perl6
+       rest                ; Emacs as a REST client
+       ;;rst               ; ReST in peace
+       ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
+       ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       ;; (scala +lsp)     ; java, but good
+       ;;scheme            ; a fully conniving family of lisps
+       sh                  ; she sells {ba,z,fi}sh shells on the C xor
        ;;sml
        ;;solidity             ; do you need a blockchain? No.
        ;;swift                ; who asked for emoji variables?
