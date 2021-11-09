6c6b3cad55a46da2b5846c58aa07e427895227b4
Jose V. Trigueros <j.v.trigueros@gmail.com>
2021-11-09 10:45:21 -0700

    Enable javascript and scala with LSP

--- dot_doom.d/init.el
+++ dot_doom.d/init.el
@@ -135,7 +135,7 @@
        ;;idris             ;
        json              ; At least it ain't XML
        ;; (java +lsp) ; the poster child for carpal tunnel syndrome
-       ;; (javascript +lsp) ; all(hope(abandon(ye(who(enter(here))))))
+       (javascript +lsp) ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        ;;latex             ; writing papers in Emacs has never been so fun
@@ -159,7 +159,7 @@
        ;;rst               ; ReST in peace
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
-       ;; (scala +lsp)     ; java, but good
+       (scala +lsp)     ; java, but good
        ;;scheme            ; a fully conniving family of lisps
        sh                  ; she sells {ba,z,fi}sh shells on the C xor
        ;;sml
