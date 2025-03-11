8dc15848f5c596fefdeb61279ca9f9871006b7df
Jose V. Trigueros <j.v.trigueros@gmail.com>
2025-03-11 10:47:05 -0700

    Configure lsp options after lsp-mode
    Might need to install lsp-booster

--- dot_doom.d/config.el
+++ dot_doom.d/config.el
@@ -203,14 +203,50 @@
  json-mode-hook
  (setq! js-indent-level 2))
 
-;; Add custom tags for lsp-yaml
-(setq! lsp-yaml-custom-tags ["!GetAtt" "!Split" "!ImportValue" "!Ref" "!Sub" "!reference"])
+(after! lsp-mode
+  ;; Add custom tags for lsp-yaml
+  (setq! lsp-yaml-custom-tags ["!GetAtt" "!Split" "!ImportValue" "!Ref" "!Sub" "!reference"])
+  (setq!
+   ;; Disable the other terraform LSP client
+   lsp-disabled-clients '(tfls)
+   lsp-terraform-ls-enable-show-reference t
+   lsp-enable-links t)
 
-(setq!
- ;; Disable the other terraform LSP client
- lsp-disabled-clients '(tfls)
- lsp-terraform-ls-enable-show-reference t
- lsp-enable-links t)
+  (defun lsp-booster--advice-json-parse (old-fn &rest args)
+    "Try to parse bytecode instead of json."
+    (or
+     (when (equal (following-char) ?#)
+       (let ((bytecode (read (current-buffer))))
+         (when (byte-code-function-p bytecode)
+           (funcall bytecode))))
+     (apply old-fn args)))
+
+  (advice-add (if (progn (require 'json)
+                         (fboundp 'json-parse-buffer))
+                  'json-parse-buffer
+                'json-read)
+              :around
+              #'lsp-booster--advice-json-parse)
+
+  (defun lsp-booster--advice-final-command (old-fn cmd &optional test?)
+    "Prepend emacs-lsp-booster command to lsp CMD."
+    (let ((orig-result (funcall old-fn cmd test?)))
+      (message "orig-result: %s" orig-result)
+      (message "command-from-exec-path: %s" (or (+python-executable-find (car orig-result))
+                                                (executable-find (car orig-result))))
+      (if (and (not test?)                             ;; for check lsp-server-present?
+               (not (file-remote-p default-directory)) ;; see lsp-resolve-final-command, it would add extra shell wrapper
+               lsp-use-plists
+               (not (functionp 'json-rpc-connection))  ;; native json-rpc
+               (executable-find "emacs-lsp-booster"))
+          (progn
+            (when-let ((command-from-exec-path (or (+python-executable-find (car orig-result)) ;; because Python is complicated
+                                                   (executable-find (car orig-result)))))      ;; resolve command from exec-path (in case not found in $PATH)
+              (setcar orig-result command-from-exec-path))
+            (message "Using emacs-lsp-booster for %s!" orig-result)
+            (cons "emacs-lsp-booster" orig-result))
+        orig-result)))
+  (advice-add 'lsp-resolve-final-command :around #'lsp-booster--advice-final-command))
 
 (when (not (featurep :system 'macos))
   (use-package! vterm
